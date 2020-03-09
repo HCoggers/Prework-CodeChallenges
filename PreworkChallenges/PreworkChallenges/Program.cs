3d6dc82a8f9ee9c1c07954c6b9febd8df8389247
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-09 04:21:38 -0700

    Reads user input, and converts it to usable data types

--- PreworkChallenges/PreworkChallenges/Program.cs
+++ PreworkChallenges/PreworkChallenges/Program.cs
@@ -6,7 +6,25 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // ----- PROJECT 1: Array Max Result -----
+
+            //Request 5-digit array from user
+            Console.WriteLine("Hello. Please input 5 digits. You may use the same digit more than once.");
+
+            string inputOne = Console.ReadLine();
+            char[] arrayInput = inputOne.ToCharArray();
+         
+            Console.WriteLine($"You entered {inputOne}. Please select one digit from this array.");
+
+            //Request single digit selection from user
+            int inputTwo = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"You selected {inputTwo}.");
+
+            //Calculate score (digit * frequency)
+
+            //Print score
+
         }
     }
 }
