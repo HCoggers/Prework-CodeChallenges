9b90c98d16b447408c95ff9ab06a3f1323b53171
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-10 16:46:34 -0700

    mvp. Doesn't account for user error. organized satisfactorily.

--- PreworkChallenges/PreworkChallenges/Program.cs
+++ PreworkChallenges/PreworkChallenges/Program.cs
@@ -6,7 +6,26 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ----- PROJECT 3: Perfect Sequence -----
+
+            // request integer sequence
+            Console.WriteLine("Please input a sequence of numbers to check:");
+            int[] input = Array.ConvertAll(Console.ReadLine().ToCharArray(), c => (int)Char.GetNumericValue(c));
+
+            // determine if input is a perfect sequence
+            Console.WriteLine(IsPerfectSequence(input) ? "It's a Perfect Sequence!" : "Hmm... well, It's not perfect...");
+
+        }
+        static bool IsPerfectSequence(int[] sequence)
+        {
+            int product = 1;
+            int sum = 0;
+            foreach(int n in sequence)
+            {
+                product *= n;
+                sum += n;
+            }
+            return product == sum;
         }
     }
 }
