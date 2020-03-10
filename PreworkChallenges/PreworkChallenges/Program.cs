e1f25720c6d417e0e10a1bb14023a3ac8a1b2629
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-10 15:03:50 -0700

    mvp. works, doesn't account for user error.

--- PreworkChallenges/PreworkChallenges/Program.cs
+++ PreworkChallenges/PreworkChallenges/Program.cs
@@ -6,7 +6,17 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // ----- PROJECT 2: Leap Year Calculator -----
+
+            //enter a year
+            Console.WriteLine("Please enter any year you can think of:");
+            int year = Int32.Parse(Console.ReadLine());
+
+            //determine if it is a leap year
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+                Console.WriteLine("Congratulations! you found a Leap Year!");
+            else
+                Console.WriteLine("Nah, man, that's just a normal 365-day year.");
         }
     }
 }
