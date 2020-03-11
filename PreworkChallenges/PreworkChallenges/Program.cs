623f4b3fc6fefd1da5fd24d58488504aa1032cb9
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-11 15:34:14 -0700

    mvp. works with sample data. needs input method

--- PreworkChallenges/PreworkChallenges/Program.cs
+++ PreworkChallenges/PreworkChallenges/Program.cs
@@ -6,7 +6,25 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] someMatrix = new int[4, 3] { { 1, 2, 3 }, { 2, 3, 4 }, { 5, 7, 9 }, { 10, 20, 0 } };
+            
+            Console.WriteLine("Given: " + someMatrix + " The sums are:");
+            int[] result = SumMatrix(someMatrix);
+            foreach (int n in result)
+                Console.Write($"{n}, ");
+        }
+        static int[] SumMatrix(int[,] matrix)
+        {
+            int[] sum = new int[matrix.GetLength(0)];
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int indexSum = 0;
+                for (int n = 0; n < matrix.GetLength(1); n++)
+                    indexSum += matrix[i,n];
+                sum[i] = indexSum;
+            }
+            return sum;
         }
     }
 }
