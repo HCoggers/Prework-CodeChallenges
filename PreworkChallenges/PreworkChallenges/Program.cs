c65f80b41b3d4cb1e723fccd0ed50b7695b8048e
Harry Cogswell <harry.cogswell@gmail.com>
2020-03-11 16:26:52 -0700

    implements input method. needs visualization.

--- PreworkChallenges/PreworkChallenges/Program.cs
+++ PreworkChallenges/PreworkChallenges/Program.cs
@@ -6,12 +6,13 @@ namespace PreworkChallenges
     {
         static void Main(string[] args)
         {
-            int[,] someMatrix = new int[4, 3] { { 1, 2, 3 }, { 2, 3, 4 }, { 5, 7, 9 }, { 10, 20, 0 } };
+
+            int[,] someMatrix = RequestMatrix();
             
-            Console.WriteLine("Given: " + someMatrix + " The sums are:");
+            Console.WriteLine("Given your matrix The sums of each row are:");
             int[] result = SumMatrix(someMatrix);
             foreach (int n in result)
-                Console.Write($"{n}, ");
+                Console.Write($" {n} ");
         }
         static int[] SumMatrix(int[,] matrix)
         {
@@ -25,6 +26,32 @@ namespace PreworkChallenges
                 sum[i] = indexSum;
             }
             return sum;
+        }
+        static int[,] RequestMatrix()
+        {
+            Console.WriteLine("A matrix is a rectangular set of numbers. Basically, a list of lists of equal length. Let's make one!");
+            Console.WriteLine("How many Rows?");
+            int rows = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("How many Columns?");
+            int cols = Int32.Parse(Console.ReadLine());
+
+            int[,] result = new int[rows, cols];
+            Console.WriteLine($"You have an empty Matrix, {rows} by {cols}");
+            for (int r = 0; r < rows; r++)
+                for (int c = 0; c < cols; c++)
+                {
+                    string input;
+                    int output;
+                    do
+                    {
+                        Console.WriteLine($"Row {r + 1}, Column {c + 1} Please fill in a number");
+                        input = Console.ReadLine();
+                    } while (!Int32.TryParse(input, out output));
+
+                    result[r, c] = output;
+                }
+            
+            return result;
         }
     }
 }
